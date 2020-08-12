d7247be4201afa7953d44cb04c9aee27fc51d7c1
vaibhav <vaibhavbhardwaaj@gmail.com>
2020-08-12 11:01:38 +0530

    first commit

---
+++ build/precache-manifest.e55e2879ca30d935216cb2c299156802.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3f8992d0a4101766f7c1f4ae4328464",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4eae3d9427379c3f1bb5",
+    "url": "/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "26fff02250feaa384487",
+    "url": "/static/css/main.4211475e.chunk.css"
+  },
+  {
+    "revision": "4eae3d9427379c3f1bb5",
+    "url": "/static/js/2.c651d6c5.chunk.js"
+  },
+  {
+    "revision": "f25112c0081db71b0e5dd2ce64e909e6",
+    "url": "/static/js/2.c651d6c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26fff02250feaa384487",
+    "url": "/static/js/main.f8ca9cb2.chunk.js"
+  },
+  {
+    "revision": "e6de44a6c9919a7bcc38",
+    "url": "/static/js/runtime-main.ed4b9ef5.js"
+  }
+]);
